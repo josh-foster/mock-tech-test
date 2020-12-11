36e84c1628f543417dd260c7d9b798bec6dcbc9e
josh-foster <joshfoster321@hotmail.com>
2020-12-11 10:38:35 +0000

    ship objects created and added to html

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const Button = document.getElementById("attackButton");
+const defenceShipHtml = document.getElementById("defenceships")
 
-const attackShips = () => {
-    console.log("works");
+const motherShip = {
+    name: "MotherShip",
+    hitPoints: 100,
+    damgageTaken: 9
 }
+const attackShip = {
+    name: "AttackShip",
+    hitPoints: 80,
+    damgageTaken: 10
+}
+const defenceShip = {
+    name: "DeffenceShip",
+    hitPoints: 45,
+    damgageTaken: 12
+}
+
+motherships.html =  motherShip.name + motherShip.hitPoints;
+    for (var i = 0; i < motherships.length; i++) {
+        motherships[i].innerHTML = motherShip.name + motherShip.hitPoints;
+    }
+
+const defenceships = document.getElementsByClassName("defenceship");
+    for (var i = 0; i < defenceships.length; i++) {
+        defenceships[i].innerHTML = defenceShip.name + defenceShip.hitPoints;
+    }
+
+const atackships = document.getElementsByClassName("attackship");
+    for (var i = 0; i < atackships.length; i++) {
+        atackships[i].innerHTML = attackShip.name + attackShip.hitPoints;
+    }
+
